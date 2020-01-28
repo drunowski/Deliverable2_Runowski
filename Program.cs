0c32b9db94f20ec22ff013946adf50e4afa60d2d
David Runowski <drunowski@usf.edu>
2020-01-28 17:41:23 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿//Author: David Runowski
+//Date: 01/30/2020
+using System;
+
+namespace Deliverable2_Runowski
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter Your Grade as an Integer: ");
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                decimal x = decimal.Parse(input);
+
+                if (x >= 98)
+                    Console.WriteLine("Your Grade is A+");
+                else if (x >= 92)
+                    Console.WriteLine("Your Grade is A");
+                else if (x >= 90)
+                    Console.WriteLine("Your Grade is A-");
+                else if (x >= 88)
+                    Console.WriteLine("Your Grade is B+");
+                else if (x >= 82)
+                    Console.WriteLine("Your Grade is B");
+                else if (x >= 80)
+                    Console.WriteLine("Your Grade is B-");
+                else if (x >= 78)
+                    Console.WriteLine("Your Grade is C+");
+                else if (x >= 72)
+                    Console.WriteLine("Your Grade is C");
+                else if (x >= 70)
+                    Console.WriteLine("Your Grade is C-");
+                else if (x >= 68)
+                    Console.WriteLine("Your Grade is D+");
+                else if (x >= 62)
+                    Console.WriteLine("Your Grade is D");
+                else if (x >= 60)
+                    Console.WriteLine("Your Grade is D-");
+                else
+                    Console.WriteLine("Your Grade is F");
+            }
+
+            catch
+            {
+                Console.WriteLine("Please Enter a Number....");
+                Console.ReadKey(true);
+            }
+
+
+            
+
+        }
+    }
+}
